09dfe87432ba0078783989a78f7951c7d33cf0d0
RosalioMayo <150847465+RosalioMayo@users.noreply.github.com>
2024-05-14 13:56:02 -0600

    Create firewall_config.sh

---
+++ firewall_config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Simple iptables configuration script
+
+# Flush existing rules
+iptables -F
+
+# Set default policies
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Allow loopback interface
+iptables -A INPUT -i lo -j ACCEPT
+
+# Allow established connections
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow SSH
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Allow HTTP and HTTPS
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Save the rules
+iptables-save > /etc/iptables/rules.v4
+
+echo "Firewall rules configured and saved."
